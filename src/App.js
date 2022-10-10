5c2d583fcf25e0188c099b41af5e5a06c271bb7c
Nayan <nayanbhakhar7075@gmail.com>
2022-10-10 22:38:33 +0530

    header responsive with mui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-import About from "./Components/About/About";
-import ContactUS from "./Components/ContactUS/ContactUS";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
-import Portfolio from "./Components/Portfolio/Portfolio";
-import Summary from "./Components/Summary/Summary";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
-      {/* <About />
-      <Summary />
-      <Portfolio />
-      <ContactUS />
-      <Footer /> */}
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,12 +6,10 @@ import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import About from "../About/About";
 import Summary from "../Summary/Summary";
 import Portfolio from "../Portfolio/Portfolio";
@@ -41,15 +39,25 @@ function Navbar(props) {
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
+        Nayan
       </Typography>
       <Divider />
       <List>
         {navItems.map((item) => (
           <ListItem key={item.name} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
+            <Link
+              href={item.link}
+              sx={{
+                justifyContent: "center",
+                fontFamily: "Poppins Regular",
+                fontSize: "1rem",
+                color: "black ",
+                paddingRight: "0.5rem",
+                textDecoration: "none",
+              }}
+            >
               <ListItemText primary={item.name} />
-            </ListItemButton>
+            </Link>
           </ListItem>
         ))}
       </List>
@@ -100,8 +108,6 @@ function Navbar(props) {
             {navItems.map((item) => (
               <Link
                 href={item.link}
-                // variant="body2"
-                // className="links"
                 sx={{
                   fontFamily: "Poppins Regular",
                   fontSize: "1rem",
@@ -123,7 +129,7 @@ function Navbar(props) {
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
             display: { xs: "block", sm: "none" },
